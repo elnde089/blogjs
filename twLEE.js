9c41df28175f2d6b5f90684d430d9dd60ccd38bb
elnde089 <65064384+elnde089@users.noreply.github.com>
2020-06-23 15:57:49 +0700

    Update twLEE.js

--- twLEE.js
+++ twLEE.js
@@ -1,6 +1,8 @@
 var twLSN = [
 'elndigital.blogspot.com',
 'elndigital.blogspot.com',
+'elndigital.blogspot.com',
+'www.elndigital.blogspot.com',
 ];
 
-var redirectUrl = 'https://elndigital.blogspot.com';
+var redirectUrl = 'https://elndigital.blogspot.com/';
